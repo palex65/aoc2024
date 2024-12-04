17327c6d5760c0b0592f7d944379e3e7b27d322e
Pedro Pereira <593033+palex65@users.noreply.github.com>
2024-12-04 15:15:25 +0000

    Day04

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,6 +1,48 @@
+/*
+const val PREFIX = "mul("
+const val MIN_DIGS = 1
+const val MAX_DIGS = 3
+
+fun parseMult(s: String, from: Int): Int? {
+    if (!s.startsWith(PREFIX,from)) return null
+    val end1 = s.indexOf(',', from + PREFIX.length + MIN_DIGS)
+    if (end1 == -1 || end1 - (from + PREFIX.length) > MAX_DIGS) return null
+    val end2 = s.indexOf(')', end1 + 1 + MIN_DIGS)
+    if (end2 == -1 || end2 - (end1 + 1) > MAX_DIGS) return null
+    val a = s.substring(from + PREFIX.length, end1).toIntOrNull() ?: return null
+    val b = s.substring(end1 + 1, end2).toIntOrNull() ?: return null
+    return a * b
+}
+
+fun part1(mem: String): Int {
+    var sum = 0
+    var i = 0
+    while (i < mem.length) {
+        val m = parseMult(mem,i)
+        i += if (m != null) { sum += m; 8 }
+        else 1
+    }
+    return sum
+}
+
+fun part2(mem: String): Int {
+    var sum = 0
+    var i = 0
+    var enable = true
+    while (i < mem.length) {
+        val m = parseMult(mem,i)
+        i += when {
+            m != null -> { if (enable) sum += m; 8 }
+            mem.startsWith("don't()",i) -> { enable = false; 7 }
+            mem.startsWith("do()",i) -> { enable = true; 4 }
+            else -> 1
+        }
+    }
+    return sum
+}
+*/
 
 fun main() {
-
     fun part1(mem: String): Int =
         Regex("""mul\((\d{1,3}),(\d{1,3})\)""").findAll(mem).sumOf { mr ->
             val (a, b) = mr.destructured.toList().map { it.toInt() }
---
+++ src/Day04.kt
@@ -0,0 +1,63 @@
+
+typealias CharMatrix = List<String>
+
+data class Position(val row: Int, val col: Int)
+
+operator fun CharMatrix.get(p: Position) = get(p.row)[p.col]
+
+fun Position.inBoundsOf(m: CharMatrix) = row in m.indices && col in m[0].indices
+
+operator fun CharMatrix.contains(p: Position) = p.row in indices && p.col in get(0).indices
+
+fun CharMatrix.allPositions(padding: Int = 0) = 
+    (padding..indices.last-padding).flatMap { row ->
+        (padding..get(0).indices.last-padding).map { col -> Position(row, col) }
+    }
+
+data class Direction(val dRow: Int, val dCol: Int) {
+    companion object {
+        val UP = Direction(-1, 0)
+        val DOWN = Direction(1, 0)
+        val LEFT = Direction(0, -1)
+        val RIGHT = Direction(0, 1)
+        val UP_LEFT = Direction(-1, -1)
+        val UP_RIGHT = Direction(-1, 1)
+        val DOWN_LEFT = Direction(1, -1)
+        val DOWN_RIGHT = Direction(1, 1)
+        val values = listOf(UP, DOWN, LEFT, RIGHT, UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT)
+    }
+}
+
+operator fun Position.plus(dir: Direction) = Position(row + dir.dRow, col + dir.dCol)
+
+fun main() {
+    fun part1(m: CharMatrix): Int =
+        m.allPositions().sumOf { pos ->
+            Direction.values.count { dir ->
+                var p = pos
+                "XMAS".all { p.inBoundsOf(m) && m[p] == it
+                    .also { p += dir }
+                }
+            }
+        }
+
+    fun part2(m: CharMatrix): Int = m.allPositions(padding = 1)
+        .count { pos ->
+            m[pos] == 'A' && run {
+                val ul = m[pos + Direction.UP_LEFT]
+                val dr = m[pos + Direction.DOWN_RIGHT]
+                val ur = m[pos + Direction.UP_RIGHT]
+                val dl = m[pos + Direction.DOWN_LEFT]
+                (ul == 'M' && dr == 'S' || ul == 'S' && dr == 'M') &&
+                (ur == 'M' && dl == 'S' || ur == 'S' && dl == 'M')
+            }
+        }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 18)
+    check(part2(testInput) == 9)
+
+    val input = readInput("Day04")
+    part1(input).println()  // 2514
+    part2(input).println()  // 1888
+}
